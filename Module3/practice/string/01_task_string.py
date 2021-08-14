bfff19a8e5e7fd3049681d8ab94e34dbdf704b3c
paul-izum <63441928+paul-izum@users.noreply.github.com>
2021-08-14 14:30:14 +0300

    Update 01_task_string.py

--- Module3/practice/string/01_task_string.py
+++ Module3/practice/string/01_task_string.py
@@ -3,8 +3,11 @@
 # и заменять первые буквы буквами в верхнем регистре.
 # Измененные данные(имя и фамилию) вывести на экран.
 
+
 name = input("Имя: ")
 surname = input("Фамилия: ")
-
+name = name[0].upper()+name[1:]
+surname = surname[0].upper()+surname[1:]
 
 print(name, surname)
+
