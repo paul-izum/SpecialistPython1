cf8d4005e18862146e2ce0783ef7528af78527b9
paul-izum <63441928+paul-izum@users.noreply.github.com>
2021-08-26 21:09:46 +0300

    Update 01_hw_func.py

--- Module4/home_work/01_hw_func.py
+++ Module4/home_work/01_hw_func.py
@@ -4,8 +4,16 @@
 
 def lucky_ticket(ticket_number):
     # TODO: your code here
-    pass
+    first_part = ticket_number//100000 + (ticket_number//10000%10) + (ticket_number//1000)%10
+    second_part = (ticket_number//100)%10 + (ticket_number//10)%10 + ticket_number%10
+    if first_part == second_part:
+        return True
+    return False
 
+# Тестируем функцию
+print(lucky_ticket(123006))
+print(lucky_ticket(12321))
+print(lucky_ticket(436751))
 
 # Тестируем функцию
 print(lucky_ticket(123006))
