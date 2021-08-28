fc14cbee06c339b7e5847bb8c17efa5c3b1d5ef6
paul-izum <63441928+paul-izum@users.noreply.github.com>
2021-08-28 12:13:54 +0300

    Update 05_task_repeat.py

--- Module5/practice/repeat/05_task_repeat.py
+++ Module5/practice/repeat/05_task_repeat.py
@@ -1,3 +1,11 @@
 # Дано общее количество объектов для отображения на веб-сайте и количество объектов на каждой странице.
 # Вычислите, сколько объектов будет отображаться на последней странице сайта.
 
+def pagination_last_page(num_items, items_on_page):
+    if num_items % items_on_page == 0:
+        return items_on_page
+    if num_items % items_on_page > 0:
+        return num_items % items_on_page
+
+
+print(pagination_last_page(67, 10))
