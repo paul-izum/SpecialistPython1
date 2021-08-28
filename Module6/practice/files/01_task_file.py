0852a3d03007f41e99a51eba803c4dd5ffa064a2
paul-izum <63441928+paul-izum@users.noreply.github.com>
2021-08-28 15:10:50 +0300

    Update 01_task_file.py

--- Module6/practice/files/01_task_file.py
+++ Module6/practice/files/01_task_file.py
@@ -6,14 +6,33 @@
 # 3. Узнайте количество стихотворений, если известно,
 # что каждое стихотворение отделяется ровно одной пустой строкой от предыдущего и после последнего нет пустой строки
 
-path = "dir/limericks.txt"  # вместо dir подставь название папки с файлом.
+path = "data/limericks.txt"  # вместо dir подставь название папки с файлом.
 # Или удалите dir, если limericks.txt в тойже папке что и текущий файл
 
 # Открываем файл на чтение
-f = open(path, "r")
+f = open(path, "r", encoding = "utf-8")
 # В переменную line считываем строку за стройкой из файла(f)
+#1
 for line in f:
-    ...
+    line = line.rstrip()
+    print(line)
+f.close()
+#2
+f = open(path, "r", encoding = "utf-8")
+sym_count = 0
+for line in f:
+    sym_count_str = len(line.rstrip())
+    sym_count += sym_count_str
+print(sym_count)
+f.close()
+#3
+f = open(path, "r", encoding = "utf-8")
+poem_count = 0
+for line in f:
+    if len(line.rstrip()) == 0:
+        poem_count += 1
+print(poem_count)
+f.close()
 
 # Подсказка: пустые строки выглядят так "\n". Помните? Строка считывается вместе с символом переноса!
 # Применение метода "\n".rstrip() --> "" вернет вам пустую строку, строку из НУЛЯ символов.
